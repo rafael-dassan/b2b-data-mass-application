6f1e32fb677ad78bb70e40d3f98cbb85d8465361
Leonardo Saldanha <leonardo.saldanha@ab-inbev.com>
2019-04-10 16:41:49 -0300

    Implement monkey_test.sh and add README to utils.
    - The monkey_test.sh script runs the monkey test and prints the number
    of crashes that happened to the application;
    - The utils folder will hold all scripts that support the testing routine
    of the BeerTech team;
    - The README file explains briefly about the utils/ content.

---
+++ utils/monkey_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set +x
+
+# App package name
+package_name=$1
+
+# Constants
+DATE=$(date +'%d-%m-%Y_%T')
+
+# Start monkey test
+# Params:
+# - throttle: delay between events
+# - ignore-crashes: continue testing even after crashing
+# - ignore-timeouts: ignore freeze
+# - pct-touch: percentage of events that will be "touch"
+# - last param: number of events
+adb shell monkey -p ${package_name} --throttle 1000 --pct-touch 40 --ignore-crashes --ignore-timeouts 25000 | adb logcat -d *:E > $DATE.txt
+
+crash_number=$(cat $DATE.txt | grep "FATAL EXCEPTION" | wc -l)
+
+if [ $crash_number = 0 ]
+then
+    rm $DATE.txt
+fi
+
+echo Crashes: ${crash_number}
+
