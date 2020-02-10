5af2fceab54844562c7e4512a23ff5ad1e052740
José Vieira <jose.vieirajunior@ab-inbev.com>
2020-02-10 09:56:25 -0300

    Remove old files from unused mobile automated tests

--- src/test/java/pages/ios/dr/AccountListPageDrIos.java
+++
@@ -1,7 +0,0 @@
-package pages.ios.dr;
-
-import pages.ios.common.AccountListCommonPageIos;
-import pages.ios.common.LoginCommonPageIos;
-
-public class AccountListPageDrIos extends AccountListCommonPageIos {
-}
