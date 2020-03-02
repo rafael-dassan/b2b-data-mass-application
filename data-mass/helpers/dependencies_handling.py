12636acb8098f2d1aaf300320e403767f6bfb55f
José Vieira <jose.vieirajunior@ab-inbev.com>
2020-03-02 18:53:07 -0300

    Update RADME and dependencies_handling files to match the standards

--- data-mass/helpers/dependencies_handling.py
+++ data-mass/helpers/dependencies_handling.py
@@ -11,12 +11,12 @@ def install_dependencies():
     file_path = os.path.join(path, "requirements.txt")
 
     # Get a list of installed packages
-    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-    installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+    reqs = subprocess.check_output([sys.executable, "-m", "pip", "freeze"])
+    installed_packages = [r.decode().split("==")[0] for r in reqs.split()]
 
     # Check if required dependencies are installed
     # If not, install them
-    if 'requests' and 'jsonpath-rw-ext' and 'jsonpath-rw' not in installed_packages:
+    if "requests" and "jsonpath-rw-ext" and "jsonpath-rw" not in installed_packages:
         print(text.Red + "Dependencies are not installed!" + text.White +  "\nRunning installation...")
         os.system("pip3 install -r" + file_path)
         clearTerminal()
