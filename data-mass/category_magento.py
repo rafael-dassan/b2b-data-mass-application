e82f43ce5fd97aa21e6a0735f557fa49f62deb65
Thiago Fernando Costa <thiagotc@lnxcit019732.cit>
2020-06-16 15:44:51 -0300

    [BEESSR-3574] Create category on data mass

--- data-mass/category_magento.py
+++ data-mass/category_magento.py
@@ -33,6 +33,22 @@ def associate_product_to_category(country, environment, product_sku, category_id
 		return 'false'
 
 
+def create_category(country, environment, category_name, parent_id):
+	"""Create Category
+    Input Arguments:
+        - Country (BR, DO, AR, CL, ZA, CO)
+        - Environment (UAT, SIT)
+        - Category name
+		- Parent ID
+    Return category object
+    """
+	response = request_create_category(country, environment, category_name, parent_id)
+	if response.status_code == 200:
+		return loads(response.text)
+	else:
+		return 'false'
+
+
 def request_associate_product_to_category(country, environment, product_sku, category_id):
     # Get header request
 	url = "{url_base}/rest/V1/categories/{product_sku}/products".format(
@@ -75,3 +91,35 @@ def request_get_categories(country, environment, parent_id = 0):
 
 	# Send request
 	return place_request("GET", url, '', headers)
+
+
+def request_create_category(country, environment, category_name, parent_id = 0):
+	# Get header request
+	url = "{url_base}/rest/V1/categories".format(url_base=get_magento_base_url(environment, country))
+    
+	# Get base URL
+	access_token = get_magento_datamass_access_token(environment, country)
+
+	# Get header request
+	headers = {
+        "Content-Type": "application/json",
+        "x-access-token": access_token
+    }
+
+	data = {
+        "category": {
+			"parent_id": parent_id,
+			"name": category_name,
+			"is_active": True,
+			"position": 0,
+			"level": 0,
+			"children": "",
+			"available_sort_by": [],
+			"include_in_menu": True,
+			"extension_attributes": {},
+			"custom_attributes": []
+    	}
+	}
+
+	# Send request
+	return place_request("POST", url, convert_json_to_string(data), headers)
