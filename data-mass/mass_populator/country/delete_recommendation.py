5f2c47458b22d96092aaca7e2174e65067b7ea2c
José Vieira <jose.vieirajunior@ab-inbev.com>
2020-07-09 17:39:29 -0300

    Change the log info message when there is no sell up recommendation

--- data-mass/mass_populator/country/delete_recommendation.py
+++ data-mass/mass_populator/country/delete_recommendation.py
@@ -9,7 +9,7 @@ def delete_sell_up_recommendation(abi_id, zone, environment, use_case='CROSS_SEL
     up_sell_data = get_recommendation_by_account(abi_id, zone, environment, use_case)
 
     if up_sell_data == 'not_found':
-        logger.info('Skipping the removal of recommendation type sell up because the account does not have any')
+        logger.info('Skipping the removal of sell up recommendation because there is none for the account %s', abi_id)
     elif up_sell_data == 'false':
         logger.error(log(Message.RETRIEVE_RECOMMENDER_SELL_UP_ERROR, {'account_id': abi_id}))
     else:
