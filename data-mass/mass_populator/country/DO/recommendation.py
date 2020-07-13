3e002726fcabf5e8f31bbf84429901c6ac19ccc4
José Vieira <jose.vieirajunior@ab-inbev.com>
2020-07-13 09:22:37 -0300

    [BEESQM-2078] Create function to delete recommendation type sell up

--- data-mass/mass_populator/country/DO/recommendation.py
+++ data-mass/mass_populator/country/DO/recommendation.py
@@ -1,6 +1,6 @@
 from mass_populator.log import *
 from mass_populator.country.populate_recomendation import populate_recommendation
-from mass_populator.country.delete_recommendation import delete_sell_up_recommendation
+from mass_populator.country.delete_recommendation import delete_recommendation
 
 logger = logging.getLogger(__name__)
 
@@ -10,8 +10,8 @@ def populate_recomendations(environment):
     account_id_poc_1 = "9883300001"
     account_id_poc_2 = "9883300002"
 
-    delete_sell_up_recommendation(account_id_poc_1, country, environment)
-    delete_sell_up_recommendation(account_id_poc_2, country, environment)
+    delete_recommendation(account_id_poc_1, country, environment, 'CROSS_SELL_UP_SELL')
+    delete_recommendation(account_id_poc_2, country, environment, 'CROSS_SELL_UP_SELL')
 
     populate_recommendation(country, environment, account_id_poc_1)
     populate_recommendation(country, environment, account_id_poc_2)
--- data-mass/mass_populator/country/delete_recommendation.py
+++ data-mass/mass_populator/country/delete_recommendation.py
@@ -5,14 +5,15 @@ from mass_populator.log import *
 logger = logging.getLogger(__name__)
 
 
-def delete_sell_up_recommendation(abi_id, zone, environment, use_case='CROSS_SELL_UP_SELL'):
+# Delete recommendation from account
+def delete_recommendation(abi_id, zone, environment, use_case):
     up_sell_data = get_recommendation_by_account(abi_id, zone, environment, use_case)
 
     if up_sell_data == 'not_found':
-        logger.info('Skipping the removal of sell up recommendation because there is none for the account {account_id}'
-                    .format(account_id=abi_id))
+        logger.info('Recommendation type {use_case_type} not found for account {account_id}. Skipping...'
+                    .format(use_case_type=use_case, account_id=abi_id))
     elif up_sell_data == 'false':
-        logger.error(log(Message.RETRIEVE_RECOMMENDER_SELL_UP_ERROR, {'account_id': abi_id}))
+        logger.error(log(Message.RETRIEVE_RECOMMENDER_ERROR, {'use_case_type': use_case, 'account_id': abi_id}))
     else:
         if 'success' != delete_recommendation_by_id(environment, up_sell_data):
-            logger.error(log(Message.DELETE_RECOMMENDER_SELL_UP_ERROR, {'account_id': abi_id}))
+            logger.error(log(Message.DELETE_RECOMMENDER_ERROR, {'use_case_type': use_case, 'account_id': abi_id}))
