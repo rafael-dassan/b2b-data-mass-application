8899d7ce1c766b31567a01e1150e2aebed9fff10
Fabricio Fraga <root@lnb024490bhz.ciandt.global>
2020-10-08 14:28:03 -0300

    create flow to unroll pocs inside polulate

--- data-mass/mass_populator/country/populate_rewards.py
+++ data-mass/mass_populator/country/populate_rewards.py
@@ -1,8 +1,10 @@
+from rewards import *
+
 def desenroll_pocs(country, environment, dataframe_products):
     if dataframe_products is not None:
-        dataframe_products.apply(exec_denseroll_account, 
+        dataframe_products.apply(exec_denseroll_poc, 
         args=(country, environment), axis=1)
 
-
-def exec_denseroll_account(row, country, environment):
-    print(row['account_id_unroll'])
+def exec_denseroll_poc(row, country, environment):
+    account_id = str(row['account_id_unroll'])
+    delete_enroll_poc_to_program(account_id, country, environment)
