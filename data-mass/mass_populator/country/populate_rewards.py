2c5dce75ab8118ccfe7b9d9576f41c7d937a0596
Fabricio Fraga <root@lnb024490bhz.ciandt.global>
2020-10-09 15:53:13 -0300

    improvement of some description names

--- data-mass/mass_populator/country/populate_rewards.py
+++ data-mass/mass_populator/country/populate_rewards.py
@@ -2,9 +2,9 @@ from rewards import *
 
 def desenroll_pocs(country, environment, dataframe_products):
     if dataframe_products is not None:
-        dataframe_products.apply(exec_denseroll_poc, 
+        dataframe_products.apply(exec_unenroll_poc, 
         args=(country, environment), axis=1)
 
-def exec_denseroll_poc(row, country, environment):
-    account_id = str(row['account_id_unroll'])
+def exec_unenroll_poc(row, country, environment):
+    account_id = str(row['account_id_unenroll'])
     delete_enroll_poc_to_program(account_id, country, environment)
