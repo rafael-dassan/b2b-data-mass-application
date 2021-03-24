9cc4f13fe930d809dcdcddca85a59422208e61fa
Samuel Eduardo Noll <samuel.noll-ext@ab-inbev.com>
2021-03-24 09:55:52 -0300

    Adding setup.py for pip install eligibility

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='b2b-data-mass-application',
+     version='0.1',
+     scripts=[],
+     author="BEES Force Team",
+     author_email="",
+     description="BEES Data mass cloning from PRD to QAI or UAT",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://ab-inbev.visualstudio.com/GHQ_B2B_Delta/_git/b2b-data-mass-application",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )
