72f8bc05d24f11b989802795daffe4e9d0d6dc9a
Samuel Borba <Samuel.Borba-ext@ab-Inbev.com>
2021-05-07 13:41:46 +0000

    Merged PR 131584: Add Auto Update
    Thank you for your contribution to out B2B Data Mass Framework.
    
    ## Type of changes
    
    - [ ] Bug fix
    - [X] New feature
    - [ ] Documentation / docstrings
    - [ ] Tests
    - [ ] Refactoring
    - [ ] Other
    
    ## Checklist
    
    - [X] I accept that @<[GHQ_B2B_Delta]\Data Mass> is required as a PR reviewer.
    - [X] I guarantee that the proposed changes have been tested and are working without any side effects
    
    ### Documentation:
    
    - [X] Jira ticket: https://ab-inbev.atlassian.net/browse/BEESDM-97
    - [ ] Confluence page (If exists): https://ab-inbev.atlassian.net/wiki/spaces/PKB/overview
    
    ## Description
    Add auto update when creating a new environment

--- env-maker.sh
+++ env-maker.sh
@@ -15,10 +15,12 @@ MINIMUM_TARGET_VERSION="3.7"
 RECOMMENDED_VERSION="3.9"
 CURRENT_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
 VIRTUALENV_DIR=${1:-venv}
+SHOULD_INSTALL="1"
 
 # Colors
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
 CYAN=`tput setaf 6`
 RESET=`tput sgr0`
 
@@ -36,22 +38,79 @@ if [[ $MINIMUM_TARGET_VERSION > $CURRENT_VERSION ]] || [ "$(python3_exists)" == 
     fi
 fi
 
-echo "${CYAN}Installing virtualenv...${RESET}"
-python3 -m pip install virtualenv
-echo "${GREEN}virtualenv was successfully installed!${RESET}"
+if [ ! -d "venv" ] || [ ! -d $VIRTUALENV_DIR ]; then
+    echo "${CYAN}Installing virtualenv...${RESET}"
+    python3 -m pip install virtualenv
+    echo "${GREEN}virtualenv was successfully installed!${RESET}"
 
-echo "${CYAN}Virtualizing the Python environment...${RESET}"
-python3 -m virtualenv $VIRTUALENV_DIR -p $(which python3)
-echo "${GREEN}Done!${RESET}"
+    echo "${CYAN}Virtualizing the Python environment...${RESET}"
+    python3 -m virtualenv $VIRTUALENV_DIR -p $(which python3)
+    echo "${GREEN}Done!${RESET}"
 
-echo "${CYAN}Making sure that the latest pip version is being used...${RESET}"
-source "${VIRTUALENV_DIR}/bin/activate"
-python3 -m pip install --upgrade pip
-echo "${GREEN}pip was successfully upgraded!${RESET}"
+    echo "${CYAN}Making sure that the latest pip version is being used...${RESET}"
+    source "${VIRTUALENV_DIR}/bin/activate"
+    python3 -m pip install --upgrade pip
+    echo "${GREEN}pip was successfully upgraded!${RESET}"
 
-echo "${CYAN}Installing Data Mass...${RESET}"
-python3 -m pip install .
-echo "${GREEN}Done!${RESET}"
+    echo "${GREEN}Environment has been set up successfully!${RESET}"
+fi
 
-echo "${GREEN}Environment has been set up successfully!${RESET}"
-echo "| ${CYAN}NOTE: to start the virtualenv, run \"source $VIRTUALENV_DIR/bin/activate\".${RESET} |"
+echo "${CYAN}Fetching Data Mass updates...${RESET}"
+# git fetch
+
+REMOTE=$(git rev-parse origin/master)
+
+if [[ ! $(git log | grep ${REMOTE}) ]]; then
+    echo "${RED}Your version of Data Mass is out of date.${RESET}"
+
+    if [[ $({ git diff --name-only ; git diff --name-only --staged ; } | sort | uniq) ]]; then
+        read -e -p "${RED}You have changed files. Do you want to continue with the update?${RESET} [Y/n] " YN
+
+        if [[ $YN == "y" || $YN == "Y" ]]; then
+                echo "${CYAN}Stashing modified files...${RESET}"
+                git stash
+
+                echo "${YELLOW}Your modified files are stashed. To recover, use \"git stash apply\".${RESET}"
+        fi
+    fi
+
+    echo "${CYAN}Pulling updates...${RESET}"
+    git pull --rebase origin master
+    echo "${GREEN}Done!${RESET}"
+
+    if [[ $(ls .git/hooks | grep "post-merge") ]]; then
+        SHOULD_INSTALL="0"
+    fi
+fi
+
+if [ ! -d ".git/hooks/post-merge" ]; then
+cat << EOF > post-merge
+#!/usr/bin/env python3
+import sys
+from subprocess import call
+
+from data_mass.classes.text import text
+
+
+print(text.Cyan + "Updating data mass..." + text.Default)
+exit_code = call(["pip3 install ."], shell=True)
+
+if not exit_code:
+    print(text.Green + "Done!" + text.Default)
+else:
+    print(text.Red + "Error while updating Data Mass.")
+    print("Update manually by doing:\n")
+    print("pip install .")
+
+EOF
+    mv post-merge .git/hooks/
+    chmod +x .git/hooks/post-merge
+fi
+
+if [ $SHOULD_INSTALL == "1" ]; then
+    echo "${CYAN}Installing Data Mass...${RESET}"
+    python3 -m pip install .
+    echo "${GREEN}Done!${RESET}"
+fi
+
+echo "${CYAN}NOTE: to start the virtualenv, run \"source $VIRTUALENV_DIR/bin/activate\".${RESET}"
