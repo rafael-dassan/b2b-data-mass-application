16a6a3f0fc8c8e59531daea15b40277cd89fb832
Gabriel Muller <Gabriel.Muller-ext@ab-Inbev.com>
2021-05-27 20:18:17 +0000

    Merged PR 139842: Remove post install
    Thank you for your contribution to out B2B Data Mass Framework.
    
    ## Type of changes
    
    - [x] Bug fix
    - [ ] New feature
    - [ ] Documentation / docstrings
    - [ ] Tests
    - [ ] Refactoring
    - [ ] Other
    
    ## Checklist
    
    - [ ] I accept that @<[GHQ_B2B_Delta]\Data Mass> is required as a PR reviewer.
    - [ ] I guarantee that the proposed changes have been tested and are working without any side effects
    
    ### Documentation:
    
    - [ ] Jira ticket: https://ab-inbev.atlassian.net/browse/PROJ-XX
    - [ ] Confluence page (If exists): https://ab-inbev.atlassian.net/wiki/spaces/PKB/overview
    
    ## Description
    
    Please describe your changes here.
    
    Remove post install

--- setup.py
+++ setup.py
@@ -1,22 +1,8 @@
-from subprocess import call
-
 from setuptools import setup
-from setuptools.command.install import install
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        call(["pre-commit", "install"])
-
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    cmdclass={
-        "install": PostInstallCommand,
-    },
     install_requires=requirements
 )
