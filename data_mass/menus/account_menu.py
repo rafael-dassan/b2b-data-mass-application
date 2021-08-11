b6d3af0eb3919137236f5bc0b593f50196858e59
Fabiana Ferreira <Fabiana.Ferreira@AB-inbev.com>
2021-08-11 11:19:50 +0000

    Merged PR 169696: [BEESFA-285] Removing filter that was preventing PY zone to use another payme...
    Thank you for your contribution to out B2B Data Mass Framework.
    
    ## Type of changes
    
    - [ ] Bug fix
    - [x] New feature
    - [ ] Documentation / docstrings
    - [ ] Tests
    - [ ] Refactoring
    - [ ] Other
    
    ## Checklist
    
    - [ ] I accept that @<[GHQ_B2B_Delta]\Data Mass> is required as a PR reviewer.
    - [ ] I guarantee that the proposed changes have been tested and are working without any side effects
    
    ### Documentation:
    
    - [ ] Jira ticket: https://ab-inbev.atlassian.net/browse/PROJ-XX
    - [ ] Confluence page (If exists): https://ab-inbev.atlassian.net/wiki/spaces/PKB/overview
    
    ## Description
    
    Please describe your changes here.
    
    [BEESFA-285] Removing filter that was preventing PY zone to use another paymentMethod different than Cash

--- data_mass/menus/account_menu.py
+++ data_mass/menus/account_menu.py
@@ -283,7 +283,7 @@ def print_payment_method_menu(zone):
         value = switcher.get(payment_method, False)
         return value
 
-    elif zone == 'AR' or zone == 'PY':
+    elif zone == 'AR':
         payment_option = 'Choose the payment method (1. CASH): '
         payment_method = input(
             DEFAULT_TEXT_COLOR + payment_option)
